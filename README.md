12625bb4a429779a9f28d4dd32e3b9a30c37c485
Philip Kraatz <philip.kraatz@gmail.com>
2017-12-22 02:09:52 +0100

    Running version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # python-airplay-webgui
+
+A simple web GUI for the python-airplay library.
+
+## Get started
+* Download code
+* Install requirements
+
+      pip install --no-cache-dir -r requirements.txt
+
+* Add two ENV variables
+
+      SECRET_KEY=YOURSUPERSECRETKEY
+      ATVIP=IP adress of your AppleTV
+* Run
+
+      python app.py
+
+* Point your browser to <IP_OF_HOST>:49913
--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from airplay import AirPlay
+import os
 
 app = Flask(__name__)
+app.secret_key = os.getenv("")
+
+atv = None
+
+def atv_connect():
+    global atv
+    atvIP = os.getenv("ATVIP")
+
+    if atv:
+        return
+
+    try:
+        atv = AirPlay(atvIP)
+    except Exception as e:
+        print e
 
 @app.route('/')
 def index():
+    atv_connect()
     return render_template('index.html')
 
+@app.route('/play', methods=['POST'])
+def play():
+    global atv
+
+    if atv:
+        if request.form['action'] == "Play":
+            atv.play(request.form['videourl'])
+            flash('Now playing!')
+        elif request.form['action'] == "Pause":
+            atv.rate(0.0)
+            flash('Paused!')
+        elif request.form['action'] == "Resume":
+            atv.rate(1.0)
+            flash('Resumed!')
+        else:
+            atv.stop()
+            flash('Stopped!')
+
+    return redirect(url_for('index'))
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=49913)
